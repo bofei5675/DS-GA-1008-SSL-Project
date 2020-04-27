f51f39bd59d66517d0bf35d81b07594914ad49f0
cuican1432 <cc5048@nyu.edu>
2020-04-27 19:01:37 -0400

    alter loss

--- ssl/cpc/nt_xent_loss.py
+++ ssl/cpc/nt_xent_loss.py
@@ -1,29 +1,38 @@
 import torch
 
-def nt_xent_loss(X, Y_hat, Y):
-    '''
-    Input: X, Y_hat, Y are matrices with shape (batch, seq, hid_size)
-    Return: NT-Xent loss
-    '''
-    X = X.view(X.shape[0], -1)
-    Y_hat = Y_hat.view(Y_hat.shape[0], -1)
-    Y = Y.view(Y.shape[0], -1)
 
-    X_norm = X / (torch.norm(X, dim=1).reshape(-1, 1))
-    Y_norm = Y / (torch.norm(Y, dim=1).reshape(-1, 1))
-    Y_hat_norm = Y_hat / (torch.norm(Y_hat, dim=1).reshape(-1, 1))
-    YhY_norm = torch.cat([Y_hat_norm, Y_norm], dim=0)
-    YYh_norm = torch.cat([Y_norm, Y_hat_norm], dim=0)
-    XYhY_norm = torch.cat([X_norm, Y_hat_norm, Y_norm], dim=0)
+class ntxentLoss(torch.nn.Module):
 
-    sim_mat_1 = XYhY_norm @ XYhY_norm.T
-    exp_mat = torch.exp(sim_mat_1 / tau)
-    exp_mat_row_sum = torch.sum(exp_mat, dim=1)
-    exp_mat_diag = torch.diag(exp_mat)
-    denominator = (exp_mat_row_sum - exp_mat_diag)[X.shape[0]:]
+    def __init__(self, args):
+        super().__init__()
 
-    sim_mat_2 = YhY_norm @ YYh_norm.T
-    numerator = torch.exp(torch.diag(sim_mat_2) / tau)
+        self.tau = args.temperature
+        self.device = args.device
 
-    nt_xent = - torch.log(numerator / denominator)
-    return torch.mean(nt_xent)
+    def forward(self, x, y_hat, y):
+        """
+        Input: x, y_hat, y are matrices with shape (batch, seq, hid_size)
+        Return: NT-xent loss
+        """
+        x = x.view(x.shape[0], -1).to(self.device)
+        y_hat = y_hat.view(y_hat.shape[0], -1).to(self.device)
+        y = y.view(y.shape[0], -1).to(self.device)
+
+        x_norm = x / (torch.norm(x, dim=1).reshape(-1, 1))
+        y_norm = y / (torch.norm(y, dim=1).reshape(-1, 1))
+        y_hat_norm = y_hat / (torch.norm(y_hat, dim=1).reshape(-1, 1))
+        yhy_norm = torch.cat([y_hat_norm, y_norm], dim=0)
+        yyh_norm = torch.cat([y_norm, y_hat_norm], dim=0)
+        xyhy_norm = torch.cat([x_norm, y_hat_norm, y_norm], dim=0)
+
+        sim_mat_1 = xyhy_norm @ xyhy_norm.T
+        exp_mat = torch.exp(sim_mat_1 / self.tau)
+        exp_mat_row_sum = torch.sum(exp_mat, dim=1)
+        exp_mat_diag = torch.diag(exp_mat)
+        denominator = (exp_mat_row_sum - exp_mat_diag)[x.shape[0]:]
+
+        sim_mat_2 = yhy_norm @ yyh_norm.T
+        numerator = torch.exp(torch.diag(sim_mat_2) / self.tau)
+
+        nt_xent = - torch.log(numerator / denominator)
+        return torch.mean(nt_xent)
